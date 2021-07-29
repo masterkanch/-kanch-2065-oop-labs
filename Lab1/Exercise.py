339354153cdfc7fc05d270f3e5dc319c7c5b56ab
Kanch Ruansiripiyakul <49100648+masterkanch@users.noreply.github.com>
2021-07-30 02:24:54 +0700

    Delete Lab1 directory

--- Lab1/Exercise.py
+++
@@ -1,3 +0,0 @@
-a = 1
-if a:
-    print(f"A is true")
--- Lab1/Exercise1_2.py
+++
@@ -1,9 +0,0 @@
-while True:
-    try:
-        temp_cel = float(input("Enter a temperature in celsius: "))
-        temp_fah = (9/5) * temp_cel + 32
-        print(f"{round(temp_cel,2)} in Celsius is {round(temp_fah,2)}")
-        break
-    except ValueError:
-         print("Please enter a valid floating point for temperature.")
-    
