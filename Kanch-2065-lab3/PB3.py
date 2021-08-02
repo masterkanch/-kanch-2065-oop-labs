81d98f0a7cf815ba93832df88c765ebdbccfc9e0
Kanch Ruansiripiyakul <digitazine@gmail.com>
2021-08-03 01:27:53 +0700

    fixed print bug

--- Kanch-2065-lab3/PB3.py
+++ Kanch-2065-lab3/PB3.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     for index, values in enumerate(string):
         chars.append(values)
     print(f"chars are {chars}")
-    print(f"The enterd string is Khon Kaen University and the result of convert a vowel to uppercase is")
+    print(f"The enterd string is {string} and the result of convert a vowel to uppercase is")
     check_vowel = [char.upper() for char in chars if char == 'a' or char == 'e' or char == 'i' or char ==
                    'o' or char == 'u' or char == 'A' or char == 'E' or char == 'I' or char == 'O' or char == 'U']
     print(check_vowel)
