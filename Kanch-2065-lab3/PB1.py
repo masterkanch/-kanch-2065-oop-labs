d1755ef0ed3cc89e93be76b00ff3f65ae39ca7df
Kanch Ruansiripiyakul <digitazine@gmail.com>
2021-08-03 11:04:00 +0700

    Fixed bug

--- Kanch-2065-lab3/PB1.py
+++ Kanch-2065-lab3/PB1.py
@@ -1,29 +1,37 @@
 import random
 
 if __name__ == "__main__":
-    answer = random.randint(1, 10)
-    guess_chance = 5
+    ANSWER = random.randint(1, 10)
+    GUESSES_CHANCE = 5
     while True:
         try:
             guess = int(input("Enter an integer to guess: "))
-            if guess == answer:
+            if guess == ANSWER:
                 print("Congrats that you guess the number correctly!")
                 break
-            elif guess > answer and 10 > guess > 1:
+            elif guess > ANSWER and 10 > guess >= 1:
                 print("Try a lower number")
-                guess_chance -= 1
-                print(f"You have {guess_chance} guesses remaining")
-                if guess_chance == 0:
+                GUESSES_CHANCE -= 1
+                if GUESSES_CHANCE == 0:
+                    print("I am 0")
+                    print(f"You have {GUESSES_CHANCE} guess remaining")
                     break
-                continue
-            elif guess < answer and 10 > guess > 1:
+                elif GUESSES_CHANCE < 2:
+                    print(f"You have {GUESSES_CHANCE} guess remaining")
+                else:
+                    print(f"You have {GUESSES_CHANCE} guesses remaining")
+            elif guess < ANSWER and 10 > guess >= 1:
                 print("Try a higher number")
-                guess_chance -= 1
-                print(f"You have {guess_chance} guesses remaining")
-                if guess_chance == 0:
+                GUESSES_CHANCE -= 1
+                if GUESSES_CHANCE == 0:
+                    print(f"You have {GUESSES_CHANCE} guess remaining")
                     break
-                continue
+                elif GUESSES_CHANCE < 2:
+                    print(f"You have {GUESSES_CHANCE} guess remaining")
+                else:
+                    print(f"You have {GUESSES_CHANCE} guesses remaining")
             elif guess < 1 or guess > 10:
                 print("Please guess with with an integer in the range [1,10]")
         except ValueError:
             print('Please enter an integer to guess')
+        
