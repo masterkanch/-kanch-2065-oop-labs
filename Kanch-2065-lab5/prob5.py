2a54e4646098af8a27bab77f3456bbc1be69ff83
Kanch Ruansiripiyakul <digitazine@gmail.com>
2021-08-21 16:43:06 +0700

    minor change

--- Kanch-2065-lab5/prob5.py
+++ Kanch-2065-lab5/prob5.py
@@ -50,4 +50,4 @@ if __name__ == '__main__':
     print(f"flexible_calculator(ADD, 'int', 1, 2, 3, 4) = {flexible_calculator(ADD, 'int', 1, 2, 3, 4)}")
     print(f"flexible_calculator(MUL, 'int', 2, 3, 4) = {flexible_calculator(MUL, 'int', 2, 3, 4)}")
     print(f"flexible_calculator(DIV, 'float', 10, 5, 2) = {flexible_calculator(DIV, 'float', 10, 5, 2)}")
-    print(f"flexible_calculator(DIV, 'float', 5, 0) = {flexible_calculator(DIV, 'float', 5,0)}")
+    print(f"flexible_calculator(DIV, 'float', 5, 0) = {flexible_calculator(DIV, 'float', 5, 0)}")
