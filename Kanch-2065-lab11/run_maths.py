db36a84bd51cc1f05edead19947dedf1979bbf03
Kanch Ruansiripiyakul <digitazine@gmail.com>
2021-11-05 19:22:58 +0700

    commit pb 5

---
+++ Kanch-2065-lab11/run_maths.py
@@ -0,0 +1,30 @@
+import sys
+operator_list = ['+', '-', 'x', '/']
+
+if __name__ == '__main__':
+    try:
+        num_args = len(sys.argv) - 1
+        operator = sys.argv[1]
+        operand1 = float(sys.argv[2])
+        operand2 = float(sys.argv[3])
+
+        if operator in operator_list:
+            if operator == '+':
+                print(f"{operand1} + {operand2} is {operand1 + operand2}")
+            elif operator == '-':
+                print(f"{operand1} - {operand2} is {operand1 - operand2}")
+            elif operator == 'x':
+                print(f"{operand1} x {operand2} is {operand1 * operand2}")
+            elif operator == '/':
+                if operand2 != 0:
+                    print(f"{operand1} / {operand2} = {operand1 / operand2}")
+                elif operand2 == 0:
+                    try:
+                        print(f"{operand1} cannot divide by zero")
+                    except ZeroDivisionError:
+                        print(f"{operand1} cannot be divided by {operand2}")
+    except ValueError:
+        pass
+    except IndexError:
+        pass
+
